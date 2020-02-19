9ae55b92e84a0550b93e09890e4637208fe60780
Anil <53044029+anilcygen@users.noreply.github.com>
2020-02-19 09:50:38 +0530

    react adding css

--- src/App.js
+++ src/App.js
@@ -9,12 +9,63 @@ function App() {
     "description" : " testing the page passing data from object acess the data like myblogdottitle or myblogdotdescription"
   }
 
+  // 111  assigning styles to varible and assign/calling these styles to th each div
+  const styles ={
+    margin : '16px',
+    padding : '16px',
+    boxSizing : 'border-box',
+    borderRadius : '5px',
+    boxShadow :'0 2px 5px #ccc'
+
+  }
+
 
   return (
     <div className="App">
+      {/* adding styles */}
+      <div style={
+        {
+          margin : '16px',
+          padding : '16px',
+          boxSizing : 'border-box',
+          borderRadius : '5px',
+          boxShadow :'0 2px 5px #ccc'
+
+        }
+      } >
      <h3> {myBlog.title} </h3>
      <p> {myBlog.description} </p>
+     <h3> assigning style directly in div</h3>
+     </div>
+     {/* hr  means give horizontal line  */}
+     <hr></hr>
+
+     <div style={styles}>
+     <h3> {myBlog.title} </h3>
+     <p> {myBlog.description} </p>
+     <h3>asssigning style to a varible and callign that varible and assigning to style in div</h3>
+     </div>
+     <hr></hr>
+
+     <div style={styles}>
+     <h3> {myBlog.title} </h3>
+     <p> {myBlog.description} </p>
+     </div>
+     <hr></hr>
+
+
+
+{/* how to call css style from another file like appp dot css */}
+<div class="pageborder">
+    <h3> {myBlog.title} </h3>
+     <p> {myBlog.description} </p>
+     <h3> created a class for div and in app dot css file we are creating a style how to border particular class</h3>
+</div>
+
+
+
     </div>
+    
   );
 }
 
